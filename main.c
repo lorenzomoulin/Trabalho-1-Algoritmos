19dfebbf8dbb70ba6c41111740927ec497f2db58
lorenzomoulin <lorenzomoulin@gmail.com>
2017-10-15 16:42:42 -0200

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: Daniel
- *
- * Created on 9 de Outubro de 2017, 20:53
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-
-/*
- * 
- */
 
 int main(int argc, char** argv) {
     /*ARMAZENAMENTO DENSO*/
