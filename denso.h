96bbce6d015540ae31959d7f2d1e8c98d1d187d4
lorenzomoulin <lorenzomoulin@gmail.com>
2017-10-27 23:39:12 -0200

    Update denso.h

--- denso.h
+++ denso.h
@@ -4,48 +4,70 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//gera b a partir da funcao do pdf
 float** generate_b(float** A);
 
+//aloca espaco para matrix
 float** create_matrix();
 
+//imprime o conteudo de um vetor
 void print_vector(float** vector);
 
+//cria matrix de permutacoes
 float** create_matrix_P();
 
+//imprime conteudo de matrix
 void print_matrix(float** matrix);
 
+//multiplica matrix por vetor
 float** multi_matrix_vector(float** matrix, float** vector);
 
+//copia conteudo de matrix para outra
 void copy_matrix(float** matrix, float** copy);
 
+//le as informacoes do arquivo
 float ** read_matrix_MatrixMarket(FILE * file);
 
+//calcula modulo de um valor
 float mod(float n);
+
 
 float** LU_solution(float** L, float** U, float** P, float** b); 
 
+//faz Ux=y
 float** solution_Ux_y(float** U, float** y);
 
+//faz Ly=Pb
 float** solution_Ly_Pb(float** L, float** P, float** b);
 
+//encontra pivo de uma coluna
 float pivot(float** matrix, int j);
 
+//troca duas linhas
 void swap(float** matrix, int row1, int row2);
 
+//adiciona uma linha a outra
 void add_row(float** matrix, int row1, int row2, float k);
 
+//verifica se matrix foi corretamente eliminada
 int eliminated(float** matrix);
 
+//cria matrix triangular inferior
 void create_utriangular(float** matrix);
 
+//zera valores da coluna que estao abaixo do pivo
 void clear_column(float** matrix, float** L, int column, float pivot);
 
+//aplica o metodo de gauss
 void gauss_elimination(float** matrix, float** L, float** P);
 
+//foi usada apenas no comeco, nao deu tempo de apagar
 float** transpose_matrix(float** matrix);
 
+//desaloca espaco para matrix
 void destroy_matrix(float** matrix);
 
+//calcula norma infinita de v
 float max(float** v);
 
 int return_length_matrix();
